ff2fe7bc941a41d3dff49c2d7f95be58fc1cdbf8
alawllm <alicja.willam@gmail.com>
2023-10-19 12:28:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 💸 ServerSideRendering with Express
+
+This is a simple site, made in order to learn Server Side Rendering with Express.
+
+**Stack**
+
+- Express
+- EJS
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = 3000;
+
+    this.configureMiddleware();
+    this.configureRoutes();
+    this.serveStatic();
+  }
+
+  serveStatic() {
+    this.app.use(express.static(path.join(__dirname, "public")));
+  }
+
+  configureMiddleware() {
+    this.app.set("view engine", "ejs");
+    this.app.set("views", path.join(__dirname, "views"));
+  }
+
+  configureRoutes() {
+    this.app.get("/", (req, res) => {
+      res.render("index", { message: "Hello, SSR!" });
+    });
+
+    this.app.get("/facts", (req, res) => {
+      res.render("facts");
+    });
+
+    this.app.get("/for-against", (req, res) => {
+      res.render("for-against");
+    });
+  }
+
+  start() {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on http://localhost:${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
